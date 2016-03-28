15800eb3534e3c104a59c4c53577159f420b05ba
Aleksandr Krylov <Alexandrk86@gmail.com>
2016-03-29 01:23:01 +0300

    Исправил подсчет средней скорости

--- observer/WeatherStation/WindSensorStatistic.cpp
+++ observer/WeatherStation/WindSensorStatistic.cpp
@@ -13,14 +13,14 @@ void CWindSensorStatistic::Update(double speed, double angle)
 	}
 	else if (m_x < 0 && m_y >= 0)
 	{
-		m_average = (std::atan(m_y / m_x) + M_PI / 2) * 180 / M_PI;
+		m_average = (std::atan(std::abs(m_x) / m_y) + M_PI / 2) * 180 / M_PI;
 	}
 	else if (m_x < 0 && m_y < 0)
 	{
-		m_average = (std::atan(m_y / m_x) + M_PI) * 180 / M_PI;
+		m_average = (std::atan(std::abs(m_y) / std::abs(m_x)) + M_PI) * 180 / M_PI;
 	}
 	else
 	{
-		m_average = (std::atan(m_x / m_y) + 3 * M_PI / 2) * 180 / M_PI;
+		m_average = (std::atan(m_x / std::abs(m_y)) + 3 * M_PI / 2) * 180 / M_PI;
 	}
 }
